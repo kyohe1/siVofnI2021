e9b6c9d64f651f0c5598cc4c8ddf5f4c066ec820
Kyohei Kawamoto <61635085+kyohe1@users.noreply.github.com>
2021-05-08 02:32:04 +0900

    [W06 FIX] fixed csv path

--- W06/task2.js
+++ W06/task2.js
@@ -1,4 +1,4 @@
-d3.csv("https://kyohe1.github.io/siVofnI021/W04/data.csv")
+d3.csv("https://kyohe1.github.io/siVofnI2021/W06/data.csv")
     .then( data => {
         data.forEach( d => { d.x = +d.x; d.y = +d.y; });
 
